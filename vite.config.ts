ed82671b7e6cb0e0f2e4988e37ccd12cea75abfa
ANANAki <1066863570@qq.com>
2026-01-06 06:27:59 +0800

    修复网关硬编码

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import {resolve} from 'path'
+import {defineConfig, loadEnv} from 'vite';
 
-// noinspection TypeScriptValidateTypes
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    port: 4000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4100',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
-
+export default defineConfig(({mode}) => {
+    // 手动加载环境变量
+    const env = loadEnv(mode, process.cwd());
+    // 打印所有环境变量，确认是否加载了正确的值
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src')
+            }
+        },
+        server: {
+            port: 4000,
+            proxy: {
+                '/api': {
+                    target: env.VITE_GATEWAY_ORIGIN,
+                    changeOrigin: true,
+                    rewrite: (path: string) => path.replace(/^\/api/, '')
+                }
+            }
+        }
+    };
+});
